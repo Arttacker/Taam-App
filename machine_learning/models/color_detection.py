8854680c85c093ea186bc8dc37c7a2a74cfb3e72
Saalehh <salehadel2001@gmail.com>
2024-04-14 18:47:40 +0200

    updated card_detection & color_detection

--- machine_learning/models/color_detection.py
+++ machine_learning/models/color_detection.py
@@ -1,45 +1,38 @@
 import warnings
 from sklearn.cluster import MiniBatchKMeans
 import numpy as np
-import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-import time
 
-warnings.simplefilter(action='ignore', category=FutureWarning)
-start_time = time.time()
 
-# Load the image using mpimg.imread(). Use a raw string (prefix r) or escape the backslashes.
-image = mpimg.imread(r'Path/to/image/file')
+def extract_dominant_colors(image_path: str, n_colors: int):
+    # Suppress FutureWarning
+    warnings.simplefilter(action='ignore', category=FutureWarning)
 
-# Resize the image to reduce computation time
-image = image[::2, ::2]
+    # Load the image using mpimg.imread()
+    image = mpimg.imread(image_path)
 
-# Get the dimensions (width, height, and depth) of the image
-w, h, d = tuple(image.shape)
+    # Resize the image to reduce computation time
+    image = image[::2, ::2]
 
-# Reshape the image into a 2D array, where each row represents a pixel
-pixel = np.reshape(image, (w * h, d))
+    # Get the dimensions (width, height, and depth) of the image
+    w, h, d = tuple(image.shape)
 
-# Set the desired number of colors for the image
-n_colors = 5
+    # Reshape the image into a 2D array, where each row represents a pixel
+    pixel = np.reshape(image, (w * h, d))
 
-# Create a MiniBatchKMeans model with the specified number of clusters and fit it to the pixels
-model = MiniBatchKMeans(n_clusters=n_colors, random_state=42, batch_size=1000).fit(pixel)
+    # Create a MiniBatchKMeans model with the specified number of clusters and fit it to the pixels
+    model = MiniBatchKMeans(n_clusters=n_colors, random_state=42, batch_size=1000).fit(pixel)
 
-# Get the cluster centers (representing colors) from the model
-colour_palette = np.uint8(model.cluster_centers_)
+    # Get the cluster labels and counts
+    labels, counts = np.unique(model.labels_, return_counts=True)
 
-# Convert RGB values to hex format
-hex_palette = ['#{:02x}{:02x}{:02x}'.format(r, g, b) for r, g, b in colour_palette]
+    # Get the cluster centers (representing colors) from the model
+    colour_palette = np.uint8(model.cluster_centers_)
 
-# Display the color palette as an image
-plt.imshow([colour_palette])
+    # Convert RGB values to hex format
+    hex_palette = ['#{:02x}{:02x}{:02x}'.format(r, g, b) for r, g, b in colour_palette]
 
-# Show the plot
-# plt.show()
+    # Sort hex_palette based on counts in descending order
+    sorted_hex_palette = [x for _, x in sorted(zip(counts, hex_palette), reverse=True)]
 
-end_time = time.time()
-execution_time = end_time - start_time
-print("Execution Time: {:.2f} seconds".format(execution_time))
-print("Color Palette (RGB):", colour_palette)
-print("Color Palette (Hex):", hex_palette)
+    return sorted_hex_palette
