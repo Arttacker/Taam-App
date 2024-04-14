5fe9832b4be252eeb45c949726ad1c5f634d2f78
Saalehh <salehadel2001@gmail.com>
2024-04-14 18:50:41 +0200

    added quality assessment

---
+++ machine_learning/models/quality_assessment.py
@@ -0,0 +1,27 @@
+from PIL import Image
+from brisque import BRISQUE
+
+
+def calculate_BRISQUE_score(image_path):
+    # Create a BRISQUE object
+    obj = BRISQUE()
+
+    # Open the image
+    image = Image.open(image_path)
+
+    # Convert the image to a color image with three channels
+    color_image = Image.new("RGB", image.size)
+    color_image.paste(image)
+
+    # Calculate the BRISQUE score for the color image
+    score = obj.score(color_image)
+
+    return score
+
+
+def asses_image_quality(image_path: str) -> bool:
+    score = calculate_BRISQUE_score(image_path)
+    return score < 42
+
+
+
